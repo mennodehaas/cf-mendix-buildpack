a763b6b3079d484f66bb7fc525b80101a332c3ed
Menno de Haas <menno.dehaas@webflight.nl>
2018-10-11 09:26:53 +0200

    set to debug

--- lib/buildpackutil.py
+++ lib/buildpackutil.py
@@ -238,7 +238,7 @@ def mkdir_p(path):
 
 def get_buildpack_loglevel():
     if os.getenv("BUILDPACK_XTRACE", "false") == "true":
-        return logging.TRACE
+        return logging.DEBUG
     else:
         return logging.INFO
 
